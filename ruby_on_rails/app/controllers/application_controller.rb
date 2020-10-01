802cbd6ecaa4be4a4ad2be663232db1a5164a22c
Jay Parekh <jay@MacBook-Pro.local>
2020-10-01 15:11:28 +0530

    fix syntactical bug

--- ruby_on_rails/app/controllers/application_controller.rb
+++ ruby_on_rails/app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
     before_action :init_ik
 
     def init_ik
-        private_key: "<your-private-key>",
-        public_key: "<your-public-key>",
+        private_key = "<your-private-key>",
+        public_key = "<your-public-key>",
         # default value for endpoint is: https://ik.imagekit.io/<IMAGEKIT_ID>/
-        url_endpoint: "<endpoint-url>"
+        url_endpoint = "<endpoint-url>"
 
         # here we initialize an instance of the SDK, which we will use to work with images
         @imagekitio = ImageKit::ImageKitClient.new(private_key, public_key, url_endpoint)
