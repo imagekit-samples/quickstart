fc841661d879880482622f2a15639b06a927ffd2
vaibhavbansal23 <vaibhavbansal23@gmail.com>
2020-10-07 01:40:44 +0530

    fix

--- react-native/app/config/imagekit.js
+++ react-native/app/config/imagekit.js
@@ -1,3 +1,7 @@
-module.exports.urlEndpoint = "https://ik.imagekit.io/demo"; //insert your own url end point here
-module.exports.publicKey = ""; //insert your own public key here
-module.exports.authenticationEndpoint = "http://localhost:8080/auth"; //your auth api path, currently set to demo server included with this project
+const urlEndpoint = "https://ik.imagekit.io/demo"; //insert your own url end point here
+const publicKey = ""; //insert your own public key here
+const authenticationEndpoint = "http://localhost:8080/auth"; //your auth api path, currently set to demo server included with this project
+
+module.exports.urlEndpoint = urlEndpoint;
+module.exports.publicKey = publicKey;
+module.exports.authenticationEndpoint = authenticationEndpoint;
