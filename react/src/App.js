8c5df0ee709c49724f599a09474eeb72693599c4
Vishal <vishal@Vishals-MacBook-Pro.local>
2022-08-10 17:27:55 +0530

    Added examples according to test cases

--- react/src/App.js
+++ react/src/App.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
-import { IKContext, IKImage, IKImgUpload } from 'imagekitio-react';
+import { IKContext, IKImage, IKUpload } from 'imagekitio-react';
 
 const publicKey = process.env.REACT_APP_PUBLIC_KEY;
 const urlEndpoint = process.env.REACT_APP_URL_ENDPOINT;
 const authenticationEndpoint = 'http://localhost:3001/auth';
 
 const App = () => {
+  const [imgTr, setImgTr] = useState([{ height: 300, width: 300 }])
   const [uploadedImageSource, setUploadedImageSource] = useState();
+  const [error, setError] = useState();
 
   const onError = err => {
     console.log("Error", err);
+    setError({ uploadFileErr: err.message })
   };
 
   const onSuccess = res => {
@@ -22,149 +25,70 @@ const App = () => {
 
   return (
     <div className="App">
-      <IKContext
-        publicKey={publicKey}
+      <h1>Low-quality placeholders without lazy-loading</h1>
+        <IKImage
         urlEndpoint={urlEndpoint}
-        authenticationEndpoint={authenticationEndpoint}
-      >
-        <p>File upload along with upload API options - To use this funtionality please remember to setup the server</p>
-        <IKImgUpload
-          publicKey={publicKey}
-          authenticationEndpoint={authenticationEndpoint}
-          urlEndpoint={urlEndpoint}
-          fileName="test.jpg"
-          tags={["sample-tag1", "sample-tag2"]}
-          customCoordinates={"10,10,10,10"}
-          isPrivateFile={false}
-          useUniqueFileName={true}
-          responseFields={["tags"]}
-          folder={"/test"}
-          onError={onError}
-          onSuccess={onSuccess}
-        />
-
-        <p>Your uploaded file will appear here </p>
-        <IKImage urlEndpoint={urlEndpoint} src={uploadedImageSource} />
-      </IKContext>
-
-      <IKContext urlEndpoint={urlEndpoint}>
-        <h2>Rendering image</h2>
-        <IKImage path="default-image.jpg" width="400" />
-
-        <h2>Loading image from an absolute path</h2>
-        <IKImage
-          src="https://ik.imagekit.io/demo/default-image.jpg"
-          width="400"
-        />
-
-        <h2>Height and width manipulation</h2>
-        <IKImage
           path="default-image.jpg"
+        className={"lqip"}
           transformation={[{
             height: 200,
-            width: 200
-          }]}
-        />
-
-        <h2>Crop mode</h2>
-        <IKImage
-          path="default-image.jpg"
-          transformation={[{
-            height: 300,
             width: 200,
-            cropMode: 'extract',
-          }]}
-        />
+        }]} />
 
-        <h2>Quality manipulation</h2>
+      <h1>Combining lazy loading with low-quality placeholders</h1>
         <IKImage
+        urlEndpoint={urlEndpoint}
           path="default-image.jpg"
-          transformation={[{ quality: 10 }]}
-          width="400"
+        transformation={[{ height: 200, width: 200 }, { q: 20, bl: 10 }]}
+        className={"lazyload-lqip"}
+        lqip={{ active: true }}
+        loading="lazy"
         />
 
-        <h2>Chained transformation</h2>
-        <h3>Step 1: Resized and cropped</h3>
+      <h1>Change Transformation by clicking on below button</h1>
         <IKImage
+        urlEndpoint={urlEndpoint}
           path="default-image.jpg"
-          transformation={[{
-            height: 300,
-            width: 200
-          }]}
+        transformation={imgTr}
+        className={"img-transformation-direct"}
         />
 
-        <h3>Step 2: Resized and cropped, then rotated</h3>
-        <IKImage
-          path="default-image.jpg"
-          transformation={[{
-            height: 300,
-            width: 200,
-          }, {
-            rt: 90,
-          }]}
+      <button className='btn-to-change-tr-direct' onClick={(e) => {
+        e.preventDefault()
+        setImgTr([{ height: 200, width: 600, rt: 180 }, { ot: 'TEST', oy: 50, ox: 100, otc: '10C0F0' }])
+      }}>Click To Transform IMG</button>
+
+      <br />
+      <br />
+
+      <h1>File Upload Demo</h1>
+      <IKUpload
+        publicKey={publicKey}
+        authenticationEndpoint={authenticationEndpoint}
+        urlEndpoint={urlEndpoint}
+        className={"file-upload-ik"}
+        fileName="test.jpg"
+        tags={["sample-tag1", "sample-tag2"]}
+        customCoordinates={"10,10,10,10"}
+        isPrivateFile={false}
+        useUniqueFileName={true}
+        responseFields={["tags"]}
+        folder={"/test"}
+        onError={onError}
+        onSuccess={onSuccess}
         />
 
-        <h3>Step 3: Rotated, then resized and cropped</h3>
-        <IKImage
-          path="default-image.jpg"
-          transformation={[{
-            rt: 90,
-          }, {
-            height: 300,
-            width: 200,
-          }]}
-        />
+      <br />
+      <br />
 
-        <h2>Adding text overlay to image</h2>
-        <IKImage
-          path="default-image.jpg"
-          transformation={[{
-            height: 300,
-            width: 300,
-            overlayText: 'ImageKit',
-            overlayTextFontSize: 50,
-            overlayTextColor: '0651D5',
-          }]}
-        />
+      {error?.uploadFileErr && <p style={{ color: 'red' }} className='upload-error-ik'>{error.uploadFileErr}</p>}
 
-        <h2>Lazy loading images</h2>
-        <IKImage
-          path="default-image.jpg"
-          transformation={[{ height: 300, width: 400 }]}
-          loading="lazy"
-          height="300"
-          width="400"
-        />
-
-        <h2>Blurred image placeholder</h2>
-        <IKImage
-          path="default-image.jpg"
-          lqip={{ active: true, quality: 20 }}
-          width="400"
-        />
-
-        <h3>Combining lazy loading with low-quality placeholders</h3>
-        <IKImage
-          path="default-image.jpg"
-          transformation={[{ height: 300, width: 400 }]}
-          lqip={{ active: true }}
-          loading="lazy"
-          height="300"
-          width="400"
-        />
-
-        <h3>disabled low-quality placeholders</h3>
         <IKImage
           urlEndpoint={urlEndpoint}
-          className={'lazyload-lqip'}
-          path={path}
-          transformation={[{
-            "height": "200",
-            "width": "200"
-          }]}
-          lqip={{ active: false }}
+        src={uploadedImageSource}
+        transformation={[{ height: 200, width: 200 }]}
+        className={"uploaded-img-ik"}
         />
-      </IKContext>
     </div>
   );
 }
