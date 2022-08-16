109e084bb07d56166105a01671900662b0851d5f
Vishal <vishal@Vishals-MacBook-Pro.local>
2022-08-16 16:46:20 +0530

    Addes samples for video

--- react/src/App.js
+++ react/src/App.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-import { IKContext, IKImage, IKUpload, IKCore } from 'imagekitio-react';
+import { IKContext, IKImage, IKUpload, IKCore, IKVideo } from 'imagekitio-react';
 
 const publicKey = process.env.REACT_APP_PUBLIC_KEY;
 const urlEndpoint = process.env.REACT_APP_URL_ENDPOINT;
 const authenticationEndpoint = 'http://localhost:3001/auth';
 
 const App = () => {
+  let reftest = useRef(null)
   const [imgIkcore, setImgIkCore] = useState('');
+  const [videoThumbSrc, setVideoThumbSrc] = useState('');
   const [imgTr, setImgTr] = useState([{ height: 300, width: 300 }])
   const [uploadedImageSource, setUploadedImageSource] = useState();
   const [error, setError] = useState();
@@ -48,8 +50,8 @@ const App = () => {
   return (
     <div className="App">
       <h1>Low-quality placeholders without lazy-loading</h1>
+      <IKContext publicKey={publicKey} authenticationEndpoint={authenticationEndpoint} urlEndpoint={urlEndpoint}>
         <IKImage
-        urlEndpoint={urlEndpoint}
           path="default-image.jpg"
         className={"lqip"}
           transformation={[{
@@ -59,7 +61,6 @@ const App = () => {
 
       <h1>Combining lazy loading with low-quality placeholders</h1>
         <IKImage
-        urlEndpoint={urlEndpoint}
           path="default-image.jpg"
         transformation={[{ height: 200, width: 200 }, { q: 20, bl: 10 }]}
         className={"lazyload-lqip"}
@@ -69,33 +70,31 @@ const App = () => {
 
       <h1>Change Transformation by clicking on below button</h1>
         <IKImage
-        urlEndpoint={urlEndpoint}
           path="default-image.jpg"
         transformation={imgTr}
         className={"img-transformation-direct"}
         />
 
+        <h3>Click to transform above image</h3>
       <button className='btn-to-change-tr-direct' onClick={(e) => {
         e.preventDefault()
         setImgTr([{ height: 200, width: 600, rt: 180 }, { ot: 'TEST', oy: 50, ox: 100, otc: '10C0F0' }])
-      }}>Click To Transform IMG</button>
+        }}>Transform</button>
 
       <br />
       <br />
 
       <h1>File Upload Demo</h1>
       <IKUpload
-        publicKey={publicKey}
-        authenticationEndpoint={authenticationEndpoint}
-        urlEndpoint={urlEndpoint}
         className={"file-upload-ik"}
-        fileName="test.jpg"
+          fileName="test.mp4"
         tags={["sample-tag1", "sample-tag2"]}
         customCoordinates={"10,10,10,10"}
         isPrivateFile={false}
         useUniqueFileName={true}
         responseFields={["tags"]}
         folder={"/test"}
+          inputRef={reftest}
         onError={onError}
         onSuccess={onSuccess}
         />
@@ -106,11 +105,36 @@ const App = () => {
       {error?.uploadFileErr && <p style={{ color: 'red' }} className='upload-error-ik'>{error.uploadFileErr}</p>}
 
         <IKImage
-          urlEndpoint={urlEndpoint}
         src={uploadedImageSource}
         transformation={[{ height: 200, width: 200 }]}
         className={"uploaded-img-ik"}
         />
+
+        <h3>Video Thumbnail</h3>
+        {videoThumbSrc && <img src={videoThumbSrc} className="video-thumbnail-img" />}
+
+        {/* { l:['image', 'i-default-image.jpg', 'l-text', 'i-helloworld', 'l-end', 'l-end'] } */}
+        <h1>Video Element</h1>
+        <IKVideo
+          className='ikvideo-with-thumbnail'
+          path={'test/test111.mp4'}
+          transformation={[{ height: 200, width: 200, q: 50 }]}
+          controls={true}
+          thumbnailTransformation={[{ height: 200, width: 200 }]}
+          onThumbnailLoad={(url) => setVideoThumbSrc(url)}
+        />
+
+        <h1>GIF To MP4 Demo</h1>
+        <IKVideo
+          className='ikvideo-gif-to-mp4'
+          path={'test/sample.gif'}
+          controls={true}
+          enabledGif={true}
+        />
+      </IKContext>
+
+      <h1>Custom Upload Button</h1>
+      {reftest && <button onClick={() => reftest.current.click()}>Upload</button>}
 
       <h1>Render Image Using IKCore Sdk</h1>
       {imgIkcore && <img src={imgIkcore} className="image-ikcore" />}
