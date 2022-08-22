cd9a4832697c14a2945b6e0ebf87978fdc443ec2
Vishal <vishal@Vishals-MacBook-Pro.local>
2022-08-22 17:38:44 +0530

    Updated sample react app with new modification lib

--- react/src/App.js
+++ react/src/App.js
@@ -9,14 +9,24 @@ const authenticationEndpoint = 'http://localhost:3001/auth';
 const App = () => {
   let reftest = useRef(null)
   const [imgIkcore, setImgIkCore] = useState('');
-  const [videoThumbSrc, setVideoThumbSrc] = useState('');
+  const [imgContextIkcore, setImgContextIkCore] = useState('');
   const [imgTr, setImgTr] = useState([{ height: 300, width: 300 }])
   const [uploadedImageSource, setUploadedImageSource] = useState();
   const [error, setError] = useState();
+  const path = "default-image.jpg";
+  const customXHR = new XMLHttpRequest();
+  customXHR.upload.addEventListener('progress', function (e) {
+      console.log("File uploading in progress")
+  });
 
   useEffect(() => {
     createCustomeImg()
   }, [])
+
+  const onStart = (file, xhr) => {
+    console.log("file", file)
+    console.log("xhr", xhr)
+  }
 
   const onError = err => {
     console.log("Error", err);
@@ -49,42 +59,19 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Low-quality placeholders without lazy-loading</h1>
       <IKContext publicKey={publicKey} authenticationEndpoint={authenticationEndpoint} urlEndpoint={urlEndpoint}>
-        <IKImage
-          path="default-image.jpg"
-        className={"lqip"}
-          transformation={[{
-            height: 200,
-            width: 200,
+
+        <p>Chained transformation</p>
+        <IKImage path={path} transformation={[{
+          "height": "200",
+          "width": "200",
+        },
+        {
+          "rotation": "90"
         }]} />
 
-      <h1>Combining lazy loading with low-quality placeholders</h1>
-        <IKImage
-          path="default-image.jpg"
-        transformation={[{ height: 200, width: 200 }, { q: 20, bl: 10 }]}
-        className={"lazyload-lqip"}
-        lqip={{ active: true }}
-        loading="lazy"
-        />
-
-      <h1>Change Transformation by clicking on below button</h1>
-        <IKImage
-          path="default-image.jpg"
-        transformation={imgTr}
-        className={"img-transformation-direct"}
-        />
-
-        <h3>Click to transform above image</h3>
-      <button className='btn-to-change-tr-direct' onClick={(e) => {
-        e.preventDefault()
-        setImgTr([{ height: 200, width: 600, rt: 180 }, { ot: 'TEST', oy: 50, ox: 100, otc: '10C0F0' }])
-        }}>Transform</button>
-
-      <br />
-      <br />
-
-      <h1>File Upload Demo</h1>
+        <h1>ImageKit React quick start</h1>
+        <h3>File upload</h3>
       <IKUpload
         className={"file-upload-ik"}
           fileName="test.mp4"
@@ -95,6 +82,8 @@ const App = () => {
         responseFields={["tags"]}
         folder={"/test"}
           inputRef={reftest}
+          xhr={customXHR}
+          onStart={onStart}
         onError={onError}
         onSuccess={onSuccess}
         />
@@ -102,7 +91,15 @@ const App = () => {
       <br />
       <br />
 
-      {error?.uploadFileErr && <p style={{ color: 'red' }} className='upload-error-ik'>{error.uploadFileErr}</p>}
+        <h3>Upload invalid file</h3>
+        <IKUpload
+          className={"file-upload-error"}
+          folder={"/test"}
+          onError={onError}
+          onSuccess={onSuccess}
+        />
+
+        {error?.uploadFileErr && <p style={{ color: 'red' }} className='upload-error-ik'>{'Your request contains invalid file type.'}</p>}
 
         <IKImage
         src={uploadedImageSource}
@@ -110,26 +107,20 @@ const App = () => {
         className={"uploaded-img-ik"}
         />
 
-        <h3>Video Thumbnail</h3>
-        {videoThumbSrc && <img src={videoThumbSrc} className="video-thumbnail-img" />}
-
-        {/* { l:['image', 'i-default-image.jpg', 'l-text', 'i-helloworld', 'l-end', 'l-end'] } */}
         <h1>Video Element</h1>
         <IKVideo
-          className='ikvideo-with-thumbnail'
-          path={'test/test111.mp4'}
-          transformation={[{ height: 200, width: 200, q: 50 }]}
+          className='ikvideo-default'
+          path={'test/default-video.mp4'}
+          transformation={[{ height: 200, width: 200 }]}
           controls={true}
-          thumbnailTransformation={[{ height: 200, width: 200 }]}
-          onThumbnailLoad={(url) => setVideoThumbSrc(url)}
         />
 
-        <h1>GIF To MP4 Demo</h1>
+        <h1>Change Video Transformation by clicking on below button</h1>
         <IKVideo
-          className='ikvideo-gif-to-mp4'
-          path={'test/sample.gif'}
+          className='ikvideo-chng-tr'
+          path={'test/default-video.mp4'}
+          transformation={[{ height: 200, width: 600, b: '5_red', q: 95 }]}
           controls={true}
-          enabledGif={true}
         />
       </IKContext>
 
@@ -138,6 +129,77 @@ const App = () => {
 
       <h1>Render Image Using IKCore Sdk</h1>
       {imgIkcore && <img src={imgIkcore} className="image-ikcore" />}
+
+      <h1>Change Transformation by clicking on below button, Outside Context</h1>
+        <IKImage
+          urlEndpoint={urlEndpoint}
+          path="default-image.jpg"
+          transformation={imgTr}
+          className={"img-transformation-direct"}
+        />
+
+        <h3>Click to transform above image</h3>
+        <button className='btn-to-change-tr-direct' onClick={(e) => {
+          e.preventDefault()
+          setImgTr([{ height: 200, width: 600, r: 'max' }, { height: 200, width: 200, rt: 180 }, { ot: 'TEST', oy: 50, ox: 100, otc: '10C0F0' }])
+        }}>Transform</button>
+
+        <br />
+        <br />
+
+      <IKContext publicKey={publicKey} authenticationEndpoint={authenticationEndpoint} urlEndpoint={urlEndpoint}>
+        <h1>Change Transformation by clicking on below button, Inside Context</h1>
+        <IKImage
+          path="default-image.jpg"
+          transformation={imgContextIkcore ? imgContextIkcore : [{ height: 200, width: 200 }]}
+          className={"img-transformation"}
+        />
+
+        <h3>Click to transform above image</h3>
+        <button className='btn-to-change-tr' onClick={(e) => {
+          e.preventDefault()
+          setImgContextIkCore([{ height: 200, width: 200, r: 'max' }])
+        }}>Transform</button>
+
+        <br />
+        <br />
+
+        <p>Lazy loading image</p>
+          <IKImage
+            className={'lazyload'}
+            path={path}
+            transformation={[{
+              "height": "200",
+              "width": "200"
+            }]}
+            loading="lazy"
+          />
+
+        <p>Progressive image loading wihtout lazy loading</p>
+        <IKImage
+          className={'lqip'}
+          path={path}
+          transformation={[{
+            "height": "200",
+            "width": "200"
+          }]}
+          onError={(e) => {
+          }}
+          lqip={{ active: true, quality: 20, blur: 10 }}
+        />
+
+        <p>Progressive image loading with lazy loading</p>
+        <IKImage
+          className={'lazyload-lqip'}
+          path={path}
+          transformation={[{
+            "height": "200",
+            "width": "200"
+          }]}
+          loading="lazy"
+          lqip={{ active: true, quality: 20, blur: 30 }}
+        />
+      </IKContext>
     </div>
   );
 }
