f83575934edd2bfddb711a3f32f0651c611a8017
Nikhil Sejwal <nikhilsejwal@Nikhils-MacBook-Air.local>
2023-09-26 14:46:21 +0530

    libs revert

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
 import { ImagekitioAngularModule } from 'imagekitio-angular';
 
 @NgModule({
