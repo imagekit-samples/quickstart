cc86c056edf95cc728031ba8ffe81a3b5eb7f2ca
ANKUR DWIVEDI <ankurdwivedi@ANKURs-MacBook-Pro-2.local>
2024-01-03 21:54:09 +0530

    formatted the code

--- react-native/app/config/imagekit.js
+++ react-native/app/config/imagekit.js
@@ -1,3 +1,3 @@
 module.exports.urlEndpoint = 'https://ik.imagekit.io/demo'; //insert your own url end point here
 module.exports.publicKey = 'public_X140up/8w//8965Hp/pI8VCM6QY='; //insert your own public key here
-module.exports.authenticationEndpoint = 'http://localhost:8080/auth'; //your auth api path, currently set to demo server included with this project
+module.exports.authenticationEndpoint = 'http://localhost:8080/auth'; //your auth api path
